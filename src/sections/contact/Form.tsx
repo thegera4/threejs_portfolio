dc7019ffdf10bab35452d1079b091c683512d964
thegera4 <84020433+thegera4@users.noreply.github.com>
2025-01-14 19:41:23 -0600

    update: fixed form margin top, resize of textarea to none and disabled cursor pointer in experience timeline

--- src/sections/contact/Form.tsx
+++ src/sections/contact/Form.tsx
@@ -2,7 +2,7 @@ import {FormProps} from "../../types.ts"
 
 const Form = ({formData, isLoading, formRef, handleSubmit, handleChange, isMobile}: FormProps) => {
   return (
-    <form ref={formRef} onSubmit={handleSubmit} className={isMobile ? "mt-4" : ""}>
+    <form ref={formRef} onSubmit={handleSubmit} className={isMobile ? "mt-4" : "mt-6"}>
       <label className="space-y-3">
         <span className="field-label">Full Name:</span>
         <input required type="text" name="name" value={formData.name} onChange={handleChange} className="field-input" placeholder="John Doe"/>
@@ -13,7 +13,7 @@ const Form = ({formData, isLoading, formRef, handleSubmit, handleChange, isMobil
       </label>
       <label className="space-y-3">
         <span className="field-label">Your message:</span>
-        <textarea required name="message" value={formData.message} onChange={handleChange} rows={5} className="field-input" placeholder="Hi, I'm interested in working with you..."/>
+        <textarea required name="message" value={formData.message} onChange={handleChange} rows={5} className="field-input" placeholder="Hi, I'm interested in working with you..." />
       </label>
       <button className="field-btn w-full mt-5" type="submit" disabled={isLoading}>
         {isLoading ? 'Sending...' : 'Send Message'}

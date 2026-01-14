a76eacedc7b344042bbb069e3ebffebff262eeca
Juan Gerardo Medellin Ibarra <thegera4@hotmail.com>
2026-01-13 21:59:34 -0600

    update: fix e2e tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,8 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         setupFiles: ['./src/setupTests.ts'],
+        include: ['tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        exclude: ['tests/e2e/**/*', 'node_modules/**/*'],
         coverage: {
             provider: 'v8',
             reporter: ['text', 'json', 'html'],
